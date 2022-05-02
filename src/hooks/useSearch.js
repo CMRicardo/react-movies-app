e68431a72af6888bd46fd3b804b3966f0ec88374
Ricardo Corrales <ricardocorrales84@gmail.com>
2022-05-02 09:35:12 -0600

    Fix a typo

--- src/hooks/useSearch.js
+++ src/hooks/useSearch.js
@@ -1,8 +1,8 @@
-const APY_KEY = import.meta.env.VITE_API_KEY
+const API_KEY = import.meta.env.VITE_API_KEY
 
 export function useSearch ({ keyword, setMovies, pageNumber = 1, setLoading }) {
   setLoading(true)
-  globalThis.fetch(`https://www.omdbapi.com/?s=${keyword}&page=${pageNumber}&apikey=${APY_KEY}`)
+  globalThis.fetch(`https://www.omdbapi.com/?s=${keyword}&page=${pageNumber}&apikey=${API_KEY}`)
     .then(res => res.json())
     .then(response => setMovies(response.Search))
   setLoading(false)
